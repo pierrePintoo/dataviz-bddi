28813043a8093663cd16a18703704bf8c802085d
Pierrot <p.pinto2oliveira@gmail.com>
2020-10-20 13:05:05 +0200

    algo working à moitié

---
+++ js/index.js
@@ -0,0 +1,148 @@
+let datas = [];
+let filteredDatas = []
+let began = false;
+let requestURL = '../datas-pour-tests.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    datas = request.response
+    // console.log(datas)
+    if(began === false){
+        drawResponse(datas)
+    }
+    document.addEventListener("click", (e) => {
+        let constraintName = e.target.id
+        let constraintValue = e.target.name
+        let isChecked = e.target.checked
+        let persons = []
+        let personsToDraw = []
+        if(isChecked){
+            began = true
+            addFilters(constraintName, constraintValue)
+            personsToDraw = pushPersonsToDraw(personsToDraw, constraintName, constraintValue)
+            drawResponse(personsToDraw)
+            // for(let filterNameCount = 1; filterNameCount <= Object.keys(filters).length; filterNameCount++){
+            //     for(let filterValueCount = 1; filterValueCount <= Object.keys(filters.localisation).length; filterValueCount++){
+            //         if(filters[filterNameCount][filterValueCount] === true){
+
+            //         }
+            //     }
+            // }
+            // persons = addConstraint(datas.length, constraintName, constraintValue, datas)
+            // console.log(persons)
+        } else {
+            removeFilters(constraintName, constraintValue)
+            personsToDraw = pushPersonsToDraw(personsToDraw, constraintName, constraintValue)
+            drawResponse(personsToDraw)
+        }
+        // console.log(conditions)
+    })
+  }
+
+  let canvas = document.getElementById('canvas')
+  let c = canvas.getContext('2d')  
+
+function drawResponse(datas){
+    initCanvas()
+    drawAllPersons(datas)
+}
+
+function initCanvas() {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+  
+    c.fillStyle = 'rgba(255,0,0,0.5)';
+    c.fillRect( 0, 0, canvas.width, canvas.height);
+    c.fill()
+}
+function getRandomIntBefore255(){
+    let randomNumber = Math.floor(Math.random() * 255)
+    return randomNumber
+}
+
+function drawAllPersons(datas){
+    let x = 300
+    let y = 100
+    let r = 20
+    let randomInt = 0
+    for (let i = 0; i < datas.length; i++){
+        if(x > canvas.width - 100){
+            x = 300
+            y += 50
+        }
+        c.beginPath()
+        randomInt = getRandomIntBefore255()
+        c.fillStyle = `rgba(${randomInt}, 0, 0, 1)`;
+        c.arc(x, y, r, 0, Math.PI * 2)
+        c.fill()
+        c.closePath()
+        x += 50
+    }
+}
+
+let filters = {
+    "localisation": {
+        "Campagne" : false,
+        "Métropole" : false
+    },
+    "sexe": {
+        "M": false,
+        "F": false
+    },
+    "bac": {
+        "L" : false,
+        "ES": false,
+        "S": false
+    },
+    "avenir_lycee": 
+    {
+        "Oui": false,
+        "Non": false
+    },
+    "premiere_idee": {
+        "Oui" : false,
+        "Non" : false
+    }
+}    
+
+function addFilters(name, value){
+    filters[name][value] = true
+}
+
+function removeFilters(name, value){
+    filters[name][value] = false
+}
+
+let animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+function pushPersonsToDraw(personsToDraw, constraintName, constraintValue) {
+    let nbPersons = 0
+    for(let i = 0; i < datas.length; i++){
+        for(filterNames in filters){
+            for(filterValues in filters[filterNames]){
+                console.log(filters[filterNames][filterValues])
+                if(filters[filterNames][filterValues] === true){
+                    if(datas[i][constraintName] === constraintValue){
+                        personsToDraw.push(datas[i])
+                        nbPersons++
+                    }
+                }
+            }
+        }
+    }
+    return personsToDraw
+}
+
+for(filterNames in filters){
+    for(filterValues in filterNames){
+        if(filters[filterNames][filterValues] === true){
+            if(datas[i][constraintName] === constraintValue){
+                personsToDraw.push(datas[i])
+                console.log(datas[i])
+                nbPersons++
+            }
+        }
+    }
+}
---
+++ js/interactions.js
@@ -0,0 +1,29 @@
+// document.addEventListener("click", (e) => {
+//     // console.log(e.target.checked, e.target.name)
+//     let name = e.target.name
+//     let isChecked = e.target.checked
+//     if(isChecked){
+//         addCondition(e.target.name)
+//         if(name == "Métropole"){
+//         }
+//     } else {
+//         removeCondition(e.target.name)
+//     }
+//     // console.log(conditions)
+//     for(prop in conditions){
+//         if(conditions[prop]){
+//             console.log(conditions[prop], 'test')
+//         }
+//     }
+// })
+
+// let conditions = {}
+
+// function addCondition(name){
+//     conditions[name] = true
+// }
+
+// function removeCondition(name){
+//     conditions[name] = false
+// }
+
